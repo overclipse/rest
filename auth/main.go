a923681b20fd3551e910456d13eb0dda073378b9
Павел Земляных <pavelzemlanyh@MacBook-Pro-Pavel-2.local>
2025-09-28 07:29:15 +0300

    conf.go

---
+++ auth/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Привет я GIN",
+		})
+	})
+	
+	router.Run(":8081")
+}
